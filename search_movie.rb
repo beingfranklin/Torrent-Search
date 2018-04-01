9660b7bbc15ac64162bf9ffad4a465ccf2dd8bbb
beingfranklin <franklinanton.007@gmail.com>
2018-04-01 17:41:24 +0530

    Addded the variable movie search term

--- search_movie.rb
+++ search_movie.rb
@@ -1,2 +1,21 @@
+
 require 'rubygems'
+require 'net/http'
 require 'json'
+movie_name ='HellBoy'
+url = "https://yts.am/api/v2/list_movies.json?query_term="+ movie_name + '"'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+parsed = JSON.parse(response)
+#puts parsed
+p parsed["status"]
+puts
+puts
+i=0
+p parsed["data"]["movies"]#["0"]["title"]
+    
+puts
+puts
+
+#If Search  Successful
+
