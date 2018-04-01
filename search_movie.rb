6d46793b511b40cdac6ed741fd689eb728d77fa3
beingfranklin <franklinanton.007@gmail.com>
2018-04-02 00:06:40 +0530

    Added conditions to check query

--- search_movie.rb
+++ search_movie.rb
@@ -1,32 +1,53 @@
+#YTS Torrent search 
+
 
 require 'rubygems'
 require 'net/http'
 require 'json'
 puts "Movie You want to search for?"
-movie_name =gets.chomp.downcase.tr(" ", "-")
+user_movie_name= gets.chomp
+movie_name =user_movie_name.downcase.tr(" ", "-")
 url = "https://yts.am/api/v2/list_movies.json?query_term="+ movie_name + '"'
 uri = URI(url)
 response = Net::HTTP.get(uri)
 parsed = JSON.parse(response)
-#puts parsed
-p parsed["status"]
-puts
-puts
-movie_cnt=0
-movie_cnt = parsed["data"]["movie_count"]
-p movie_cnt
-puts
-puts
-#p parsed["data"]["movies"]#["title"]
-movies_array= parsed["data"]["movies"]
+query_response_status = parsed["status"]
 
-parsed["data"]["movies"].each do |movies|
-    
-    puts movies["title"]    
+#If Search  Successful and return status
+if query_response_status=="ok"  
+    puts
+    puts
+    movie_cnt=0
+    movie_cnt = parsed["data"]["movie_count"]
+    p movie_cnt
+    puts
+    puts
+    #p parsed["data"]["movies"]#["title"]
+    movies_array= parsed["data"]["movies"]
+        
+    if  movie_cnt>0
+        parsed["data"]["movies"].each do |movies|   
+           puts movies["title"]    
+        end
+    elsif movie_cnt==0
+        puts "Sorry !! No Movies found with #{movie_name} as name "
+    end
+    puts
+    puts
+
+elsif query_response_status=="error"
+    puts "Error!! Your Search query could not be processed"
+else 
+    puts "Unknown Error. Please report this"
 end
 
-puts
-puts
 
-#If Search  Successful
+
+
+
+
+
+
+
+
 
